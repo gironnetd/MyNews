00d4bfa4cb63a092532714982081a0d22aa015f5
Damien Gironnet <damiengironnet@gmail.com>
2019-04-29 14:40:09 +0200

    Initial commit

---
+++ app/src/main/java/com/ocr/mynews/MyNewsActivity.kt
@@ -0,0 +1,12 @@
+package com.ocr.mynews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyNewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mynews)
+    }
+}
